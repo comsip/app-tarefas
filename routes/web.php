06133b3bd8bdb224cc1257ee3be632a03dcb9e69
fredericolsdias-pixel <frederico.lsdias@gmail.com>
2026-06-23 14:48:23 -0300

    atualiza mensagens e rotas

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RelatorioController;
 
-use App\Http\Controllers\EquipeController;
-use App\Http\Controllers\ProjetoController;
+
 
 Route::get('/', function () {
     return view('welcome');});
 
 Route::resource('relatorio', RelatorioController::class);
-Route::resource('equipes', EquipeController::class);
-Route::resource('projetos', ProjetoController::class);
+
