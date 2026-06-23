fc2d388f8686cdc0ae39fe2dd565cb3ce3f2917e
comsip <comsip@gmail.com>
2026-06-23 16:21:59 -0300

    rodrigo

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,7 @@ use App\Http\Controllers\PrazoController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
-<<<<<<< HEAD
->>>>>>> origin/aluno-murilo
-*/
+git*/
 
 
 Route::get('/', function () {
